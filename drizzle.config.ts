a08a8bb550d3148dea623a50dba330117ff85cf1
wmdboke <wbtogether@163.com>
2026-01-17 07:50:43 +0800

    add auth sigin sign up

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,4 +1,8 @@
 import type { Config } from 'drizzle-kit';
+import * as dotenv from 'dotenv';
+
+// Load environment variables from .env.local
+dotenv.config({ path: '.env.local' });
 
 export default {
   schema: './db/schema.ts',
